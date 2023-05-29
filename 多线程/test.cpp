cf99fc63e29b1160119a51f1d8fb13f8bd3f27f0
panghahman <3278475092@qq.com>
2023-05-28 23:37:24 -0700

    1

--- 多线程/test.cpp
+++ 多线程/test.cpp
@@ -1,45 +1,37 @@
-#include <pthread.h>
 #include <iostream>
+#include <pthread.h>
 #include <unistd.h>
 
 using namespace std;
 
-void *thread1_run(void *args)
-{
-    while (1)
-    {
+void *thread1_run(void *args) {
+    while (1) {
         cout << "我是线程1,我正在运行" << endl;
         sleep(1);
     }
 }
 
-void *thread2_run(void *args)
-{
-    while (1)
-    {
+void *thread2_run(void *args) {
+    while (1) {
         cout << "我是线程2,我正在运行" << endl;
         sleep(1);
     }
 }
 
-void *thread3_run(void *args)
-{
-    while (1)
-    {
+void *thread3_run(void *args) {
+    while (1) {
         cout << "我是线程3,我正在运行" << endl;
         sleep(1);
     }
 }
 
-int main()
-{
+int main() {
     pthread_t t1, t2, t3;
 
     pthread_create(&t1, NULL, thread1_run, NULL);
     pthread_create(&t2, NULL, thread2_run, NULL);
     pthread_create(&t2, NULL, thread3_run, NULL);
-    while (1)
-    {
+    while (1) {
         printf("我是主线程，我正在运行\n");
         sleep(1);
     }
--- 多线程/线程分离/thread.cc
+++ 多线程/线程分离/thread.cc
@@ -1,15 +1,32 @@
-#include <iostream>
-#include <string>
-#include <pthread.h>
+#include <cstdio>
 #include <cstring>
+#include <iostream>
+#include <pthread.h>
+#include <string>
+#include <unistd.h>
 using namespace std;
-void *threadRoutine(void* args)
-{
+void *threadRoutine(void *args) {
+    string name = static_cast<const char *>(args);
+    int cnt = 5;
+    while (cnt) {
+        cout << name << ":" << cnt-- << endl;
+        sleep(1);
+    }
+    return nullptr;
 }
 
-int main()
-{
+int main() {
     pthread_t tid;
-    pthread_create(&tid, nullptr, threadRoutine, (void *)"thread 1");
+    pthread_create(&tid, nullptr, threadRoutine, (void *) "thread 1");//(void *) "thread 1表示threadRoutine的参数args
+    //pthread_detach(tid);
+    // sleep(1);
+    // pthread_detach(tid);
+    // int n = pthread_join(tid, nullptr);
+    // if (n != 0) {
+    //     cerr << "error:" << n << ":" << strerror(n) << endl;
+    // }
+
+    sleep(5);//主线程睡眠,主线程退了,整个进程就退出了
+    //cout << "主线程sleep了5结束" << endl;
     return 0;
 }
