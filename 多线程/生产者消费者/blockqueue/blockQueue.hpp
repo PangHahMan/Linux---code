947015984cb27754a1a0716fb4fff8f731f4bf70
TaeYeon <taeyeon@192.168.162.129>
2023-06-22 00:38:50 -0700

    1

--- 多线程/生产者消费者/blockqueue/blockQueue.hpp
+++ 多线程/生产者消费者/blockqueue/blockQueue.hpp
@@ -4,25 +4,63 @@
 #include <pthread.h>
 #include <queue>
 
+const int gcap = 5;
+
+//不要认为，阻塞队列只能放整数字符串之类的，也可以放对象
 template<class T>
 class BlockQueue {
 
 public:
-    BlockQueue() {
+    BlockQueue(const int cap = gcap) : _cap(cap) {
+        pthread_mutex_init(&_mutex, nullptr);
+        pthread_cond_init(&consumer_cond, nullptr);
+        pthread_cond_init(&productor_cond, nullptr);
     }
 
+    bool isfull() { return q.size() == _cap; }
+    bool isEmpty() { return q.empty(); }
     void push(const T &in) {
+        pthread_mutex_lock(&_mutex);//细节1： 一定要保证，在任何时候，都是符合条件，才进行生产
+        while (isfull()) {          //1.我们只能在临界区内部，判断临界资源是否就绪！注定了我们在当前一定是持有锁的
+
+            //2.要让线程进行休眠等待，不能持有锁等待！
+            //3.就注定了，pthread_cond_wait要有锁的释放的能力，需要将锁传到参数
+            // 我休眠(切换)了，我醒来的时候，在哪里往后执行呢？
+            pthread_cond_wait(&productor_cond, &_mutex);
+            //4.当线程醒来的时候，注定了继续从临界区内部继续运行！ 因为我是在临界区被切走的
+            //5.注定了当我们的线程被唤醒的时候，继续在pthread_cond_wait函数处向后运行,又要重新申请锁，申请成功才会彻底返回
+        }
+        //没有满的，就要让他进行生产
+        q.push(in);
+        pthread_cond_signal(&consumer_cond);//唤醒消费者
+        pthread_mutex_unlock(&_mutex);
     }
 
-    void pop(const T &out) {
+
+    void pop(T &out) {
+        pthread_mutex_lock(&_mutex);
+
+        while (isEmpty()) {
+            pthread_cond_wait(&consumer_cond, &_mutex);
+        }
+
+        out = q.front();
+        q.pop();
+
+        pthread_cond_signal(&productor_cond);//唤醒生产者
+        pthread_mutex_unlock(&_mutex);
     }
 
     ~BlockQueue() {
+        pthread_mutex_destroy(&_mutex);
+        pthread_cond_destroy(&consumer_cond);
+        pthread_cond_destroy(&productor_cond);
     }
 
 private:
     std::queue<T> q;
     int _cap;
-    pthread_mutex_t _mutex;
-    pthread_cond_t _cond;
+    pthread_mutex_t _mutex;       //互斥锁
+    pthread_cond_t consumer_cond; //消费者对应的条件变量，空就wait
+    pthread_cond_t productor_cond;//生产者对应的条件变量，满就wait
 };
--- 多线程/生产者消费者/blockqueue/main.cc
+++ 多线程/生产者消费者/blockqueue/main.cc
@@ -1,35 +1,47 @@
 #include "blockQueue.hpp"
+#include "task.hpp"
 #include <pthread.h>
 #include <sys/types.h>
 #include <unistd.h>
 using namespace std;
 
 void *consumer(void *args) {
-    BlockQueue<int> *bq = static_cast<BlockQueue<int> *>(args);
+    BlockQueue<Task> *bq = static_cast<BlockQueue<Task> *>(args);
 
     while (true) {
-        int data = 0;
+        sleep(1);
+        Task t;
         //1.将数据从blockqueue中获取 -- 获取到了数据
 
-        bq->pop(data);
+        bq->pop(t);
+        t();//拿到了x,y,op,需要调用operator()进行计算
         //2.结合某种业务逻辑，处理数据！
-        cout << "consumer data:" << data << endl;
+        cout << "consumer Task:" << t.formatArg() << t.formatRes() << endl;
     }
 }
 
 void *productor(void *args) {
-    BlockQueue<int> *bq = static_cast<BlockQueue<int> *>(args);
+    BlockQueue<Task> *bq = static_cast<BlockQueue<Task> *>(args);
+    std::string opers = "+-*/%";
     while (true) {
+        sleep(1);
         //1.先通过某种渠道获取数据
-        int data = rand() % 10 + 1;
+        //int data = rand() % 10 + 1;
+        int x = rand() % 20 + 1;
+        int y = rand() % 10 + 1;
+        char op = opers[rand() % opers.size()];
+        Task t(x, y, op);
+
         //2.将数据推送到blockqueue -- 完成生产过程
-        bq->push(data);
+        bq->push(t);
+        cout << "productor Task:" << t.formatArg() << "?" << endl;
     }
 }
 
 int main() {
     srand((uint64_t) time(nullptr) ^ getpid());//获得一个随机数
-    BlockQueue<int> *bq = new BlockQueue<int>();
+    //BlockQueue<int> *bq = new BlockQueue<int>();
+    BlockQueue<Task> *bq = new BlockQueue<Task>();
     //单生产和单消费
     pthread_t c, p;
     pthread_create(&c, nullptr, consumer, bq);
